7d4866c755d6d197c6161749ba58535d6595ea30
Dan Cruzat <thecruzat@gmail.com>
2026-04-06 09:06:12 -0700

    Reformat check plugins

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,5 +1,5 @@
-import { library, config } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library, config } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 // Import the specific icons from your Goodies and ResumeBug
 import {
@@ -11,23 +11,33 @@ import {
   faFileLines,
   faChevronRight,
   faChevronLeft,
-  faChevronDown
-} from '@fortawesome/free-solid-svg-icons'
+  faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 import {
   faLinkedin,
   faGithub,
-  faCodepen
-} from '@fortawesome/free-brands-svg-icons'
+  faCodepen,
+} from "@fortawesome/free-brands-svg-icons";
 
 // Disable adding CSS automatically since we will handle it via Nuxt
-config.autoAddCss = false
+config.autoAddCss = false;
 
 library.add(
-  faCode, faCoffee, faAddressCard, faPaperPlane,
-  faLinkedin, faGithub, faCodepen, faFloppyDisk, faFileLines, faChevronRight, faChevronLeft, faChevronDown
-)
+  faCode,
+  faCoffee,
+  faAddressCard,
+  faPaperPlane,
+  faLinkedin,
+  faGithub,
+  faCodepen,
+  faFloppyDisk,
+  faFileLines,
+  faChevronRight,
+  faChevronLeft,
+  faChevronDown,
+);
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)
-})
+  nuxtApp.vueApp.component("font-awesome-icon", FontAwesomeIcon);
+});
